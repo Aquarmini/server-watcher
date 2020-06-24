0f6eb8b069d999fad208c22baf6a455612a4c253
李铭昕 <715557344@qq.com>
2020-06-24 17:25:32 +0800

    Update

--- hyperf.php
+++ hyperf.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
@@ -8,6 +17,8 @@ date_default_timezone_set('Asia/Shanghai');
 
 ! defined('BASE_PATH') && define('BASE_PATH', getcwd());
 ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+
+putenv('SCAN_CACHEABLE=(true)');
 
 require BASE_PATH . '/vendor/autoload.php';
 
