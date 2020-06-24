ea2b96c334652f9816efa38f363018d5906f1b35
李铭昕 <715557344@qq.com>
2020-06-24 16:49:06 +0800

    Added reload.

---
+++ collector-reload.php
@@ -0,0 +1,17 @@
+<?php
+
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', 'on');
+
+error_reporting(E_ALL);
+date_default_timezone_set('Asia/Shanghai');
+
+! defined('BASE_PATH') && define('BASE_PATH', getcwd());
+! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+
+require BASE_PATH . '/vendor/autoload.php';
+
+use Hyperf\ServerWatcher\Process;
+
+$process = new Process($argv[1], $argv[2]);
+$process();
